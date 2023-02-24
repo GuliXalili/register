25b84f1e90165db10eb14fec2a54569a216ec87c
gulixalili <gulixalili2001@gmail.com>
2023-02-24 21:19:34 +0500

    superman)

---
+++ core/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import UserRegistrationForm
+
+def register(request):
+    if request.method == "POST":
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+            messages.success(request, 'Forma yaratildi')
+            return redirect('login')
+        
+    else:
+        form = UserRegistrationForm()
+    context = {
+        'form':form
+    }
+    return render(request, 'register-13.html', context)
